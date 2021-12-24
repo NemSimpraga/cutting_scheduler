0b90a720e3f2ecc69fd0e4f950c681e5465f2e13
Nemanja Simpraga <nsimpraga@iolap.com>
2021-12-24 13:24:49 +0100

    further refininig

--- testing.py
+++ testing.py
@@ -1,7 +1,5 @@
 import pandas as pd
 
-test = pd.Series([12514.2859, 12857.14305, 6599.999868, 6771.428436, 6942.857004], index=['g1', 'g2', 'g3', 'g4', 'g5'])
-
 dict_schedule = {
     5 : pd.Series([0, 0, 0, 0, 0, 0], index=['g1', 'g2', 'g3', 'g4', 'g5', 'sum']),
     1 : pd.Series([0, 0, 0, 0, 0, 0], index=['g1', 'g2', 'g3', 'g4', 'g5', 'sum']),
@@ -11,15 +9,16 @@ dict_schedule = {
     } 
 schedule = pd.DataFrame(dict_schedule)
 
-increase = pd.Series([365*2, 375*2, 385, 395, 405], index=['g1', 'g2', 'g3', 'g4', 'g5'])
-
 dict_stats = {
     'avg' : pd.Series([365, 375, 385, 395, 405], index=['g1', 'g2', 'g3', 'g4', 'g5']),
     'f' : pd.Series([0.28571429, 0.28571429, 0.14285714, 0.14285714, 0.14285714], index=['g1', 'g2', 'g3', 'g4', 'g5']),
 }
+
 stats = pd.DataFrame(dict_stats)
+increment = pd.Series([55, 55, 55, 55, 55], index=['g1', 'g2', 'g3', 'g4', 'g5'])
 
-
+global leftover
+leftover = pd.Series([0,0,0,0,0], index=['g1', 'g2', 'g3', 'g4', 'g5'])
 
 def slaughter(sl_intake):
     return sl_intake*stats['f']*stats['avg']
@@ -28,18 +27,31 @@ def cut(intake, day):
     current_cap = 0
     max_cap = 30000
     while current_cap < max_cap:
-        if current_cap + increase['g1'] < max_cap and intake['g1'] >= increase['g1']:
-            intake['g1'] -= increase['g1']; current_cap += increase['g1']; schedule[day+1]['g1'] += increase['g1'];
-    schedule[day]['sum'] = current_cap
-    return intake
+        if intake ['g1'] > 0 and current_cap < max_cap: intake['g1'] -= increment['g1']; current_cap += increment['g1']; schedule[day+1]['g1'] += increment['g1'];
+        if intake ['g1'] > 0 and current_cap < max_cap: intake['g1'] -= increment['g1']; current_cap += increment['g1']; schedule[day+1]['g1'] += increment['g1'];
+        if intake ['g2'] > 0 and current_cap < max_cap: intake['g2'] -= increment['g2']; current_cap += increment['g2']; schedule[day+1]['g2'] += increment['g2'];
+        if intake ['g2'] > 0 and current_cap < max_cap: intake['g2'] -= increment['g2']; current_cap += increment['g2']; schedule[day+1]['g2'] += increment['g2'];
+        if intake ['g3'] > 0 and current_cap < max_cap: intake['g3'] -= increment['g3']; current_cap += increment['g3']; schedule[day+1]['g3'] += increment['g3'];
+        if intake ['g4'] > 0 and current_cap < max_cap: intake['g4'] -= increment['g4']; current_cap += increment['g4']; schedule[day+1]['g4'] += increment['g4'];
+        if intake ['g5'] > 0 and current_cap < max_cap: intake['g5'] -= increment['g5']; current_cap += increment['g5']; schedule[day+1]['g5'] += increment['g5'];
+        if(intake ['g1'] < 5 and intake ['g2'] < 5 and intake ['g3'] < 5 and intake ['g4'] < 5 and intake ['g5'] < 5): break;
+    schedule[day+1]['sum'] = current_cap
+    global leftover
+    leftover = intake
+    
+#slaughter_intake = pd.Series([80,90,40,60,120])
+#slaughter_intake = pd.Series([60,70,50,100,110])
+#slaughter_intake = pd.Series([95,45,110,120,20])
+#slaughter_intake = pd.Series([82,150,40,30,78])
+slaughter_intake = pd.Series([150,60,100,45,35])
 
-slaughter_intake = pd.Series([120,70,20,100,80])
+for day, animals in enumerate(slaughter_intake):
+    cut(slaughter(animals)+leftover, day)
 
-print(slaughter(slaughter_intake[0]))
-#for day, animals in enumerate(slaughter_intake):
-#    print(cut(slaughter(animals), day+1))
-#
-#print(schedule)
+print(schedule)
+print(schedule[5]['sum']+schedule[1]['sum']+schedule[2]['sum']+schedule[3]['sum']+schedule[4]['sum'])
+
+
 
 
 
