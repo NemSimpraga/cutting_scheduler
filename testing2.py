b63d416493f1c91469b3e9b925ccd3688fceb1d1
Nemanja Šimpraga <simpraga.nemanja@gmail.com>
2021-12-26 22:41:24 +0100

    brushing up

--- testing2.py
+++ testing2.py
@@ -6,8 +6,6 @@ dict_schedule = {
     2 : pd.Series([0, 0, 0, 0, 0, 0], index=['g1', 'g2', 'g3', 'g4', 'g5', 'sum']),
     3 : pd.Series([0, 0, 0, 0, 0, 0], index=['g1', 'g2', 'g3', 'g4', 'g5', 'sum']),
     4 : pd.Series([0, 0, 0, 0, 0, 0], index=['g1', 'g2', 'g3', 'g4', 'g5', 'sum']),
-    5 : pd.Series([0, 0, 0, 0, 0, 0], index=['g1', 'g2', 'g3', 'g4', 'g5', 'sum']),
-    6 : pd.Series([0, 0, 0, 0, 0, 0], index=['g1', 'g2', 'g3', 'g4', 'g5', 'sum']),
     } 
 schedule = pd.DataFrame(dict_schedule)
 
@@ -30,26 +28,28 @@ intake = pd.Series([10,70,50,150,50])
 for day, animals in enumerate(intake):
     schedule[day]=prepare(animals)
 
-#schedule.rename(columns={0: 'Mon', 1: 'Tue', 2: 'Wen', 3: 'Thu', 4: 'Fri'}, inplace=True)
+
+print(schedule)
 next_week = schedule[4]
 schedule = schedule.shift(1,axis=1)
 schedule.fillna(0, inplace=True)
-with pd.option_context('display.max_rows', None, 'display.max_columns', None):       
-    print(schedule)
-
+print(schedule)
+schedule[5] =0
+print(schedule)
 i=0
 j=0
+
 for i in schedule:
    while schedule[i]['sum'] > 30000:
        while schedule[j+i+1]['sum'] < 30000-decrement['sum']:
            schedule[i] = schedule[i].sub(decrement, axis='rows')
            if j+i+1 <= 4:
                schedule[j+i+1] = schedule[j+i+1].add(decrement, axis='rows')
-           else: next_week += schedule[j+i+1].add(decrement, axis='rows')
+           else: next_week = next_week.add(decrement, axis='rows')
            if schedule[i]['sum'] <= 30000: break;
        j += 1;
-       with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-           print(schedule)
-with pd.option_context('display.max_rows', None, 'display.max_columns', None):       
-    print(schedule)
+       print(schedule)
+  
+print(schedule)
+print(next_week)
 
